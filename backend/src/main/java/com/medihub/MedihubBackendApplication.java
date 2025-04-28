360406e8d4fed984f71e53e1ba420751f6122b07
kamaleshwaran11 <shankarkamal52@gmail.com>
2025-04-28 14:34:24 +0530

    Initial Spring Boot Project Setup

---
+++ backend/src/main/java/com/medihub/MedihubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.medihub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedihubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedihubBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/medihub/MedihubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medihub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedihubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
